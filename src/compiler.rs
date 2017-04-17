0d2666b847d6961e0bfc0235e4903a8f24b0d4ac
evuez <helloevuez@gmail.com>
2017-04-17 11:56:45 +0200

    Implement filter function

--- src/compiler.rs
+++ src/compiler.rs
@@ -2,8 +2,11 @@ use std::vec::Vec;
 use parser::{Element, Expression, Operator};
 
 pub fn test(elements: Vec<Element>) {
-    //let r = find_expression(elements, &|e| { match e { Operator::Store => true, _ => false } });
-    //println!("{:?}", r);
+    let r = filter(&elements, &|e| { match e {
+        &Element::Expression(_) => true,
+        _ => false
+    } });
+    println!("{:?}", r);
 }
 
 fn substitute(elements: Vec<Element>) {
@@ -25,8 +28,11 @@ fn substitute(elements: Vec<Element>) {
 // }
 
 
-fn filter<F>(elements: Vec<Element>, matcher: &F) -> Vec<Element> where F: Fn(Element) -> bool {
+fn filter<'a, F>(elements: &'a Vec<Element>, matcher: &F) -> Vec<&'a Element> where F: Fn(&Element) -> bool {
     //(1..101).filter(|&v| matcher(v as i32)).collect::<Vec<i32>>()
-    elements.iter().filter(|e| matcher(e)).collect::<Vec<Element>>()
+    elements
+        .iter()
+        .filter(|e| matcher(e))
+        .collect::<Vec<&Element>>()
 }
 
